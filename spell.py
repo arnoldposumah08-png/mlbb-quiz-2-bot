e8ebef9b91596809c97c96aee4d04b34126a655a
arnoldposumah08-png <arnoldposumah08@gmail.com>
2026-04-25 23:32:32 +0700

    Create spell.py

---
+++ spell.py
@@ -0,0 +1,16 @@
+SPELLS = [
+    "Recall",
+    "Healing",
+    "Execute",
+    "Retribution",
+    "Inspire",
+    "Sprint",
+    "Revitalize",
+    "Aegis",
+    "Petrify",
+    "Purify",
+    "Flameshot",
+    "Flicker",
+    "Arrival",
+    "Vengeance"
+]
