fb04c43d26316f1fa24a4663571e89b167e1034b
arnoldposumah08-png <arnoldposumah08@gmail.com>
2026-04-25 21:27:44 +0700

    Update question.py

--- question.py
+++ question.py
@@ -1,6 +1,95 @@
 QUESTIONS = [
-    {
-        "question": "Sebutkan 3 hero Assassin MLBB",
-        "answers": ["Fanny", "Ling", "Hayabusa"]
-    }
+
+# ================= HERO ROLE =================
+
+{"question": "Sebutkan 3 hero Assassin MLBB", "answers": ["Fanny", "Ling", "Hayabusa"]},
+{"question": "Sebutkan 4 hero Tank MLBB", "answers": ["Tigreal", "Akai", "Franco", "Atlas"]},
+{"question": "Sebutkan 5 hero Mage MLBB", "answers": ["Eudora", "Kagura", "Lunox", "Cyclops", "Aurora"]},
+{"question": "Sebutkan 4 hero Marksman MLBB", "answers": ["Layla", "Miya", "Clint", "Lesley"]},
+{"question": "Sebutkan 5 hero Fighter MLBB", "answers": ["Aldous", "Chou", "Thamuz", "Paquito", "Leomord"]},
+{"question": "Sebutkan 3 hero Support MLBB", "answers": ["Rafaela", "Estes", "Angela"]},
+
+# ================= HERO POPULER =================
+
+{"question": "Sebutkan 4 hero lincah MLBB", "answers": ["Fanny", "Ling", "Lancelot", "Joy"]},
+{"question": "Sebutkan 4 hero burst damage MLBB", "answers": ["Eudora", "Aurora", "Kagura", "Vale"]},
+{"question": "Sebutkan 4 hero crowd control MLBB", "answers": ["Tigreal", "Atlas", "Khufra", "Franco"]},
+{"question": "Sebutkan 3 hero late game MLBB", "answers": ["Aldous", "Cecilion", "Lesley"]},
+
+# ================= ITEM =================
+
+{"question": "Sebutkan 4 item attack MLBB", "answers": ["Blade of Despair", "Endless Battle", "Berserker Fury", "Malefic Roar"]},
+{"question": "Sebutkan 4 item defense MLBB", "answers": ["Immortality", "Antique Cuirass", "Athena Shield", "Blade Armor"]},
+{"question": "Sebutkan 4 item magic MLBB", "answers": ["Holy Crystal", "Divine Glaive", "Clock of Destiny", "Lightning Truncheon"]},
+{"question": "Sebutkan 3 item lifesteal MLBB", "answers": ["Haas Claws", "Endless Battle", "Bloodlust Axe"]},
+{"question": "Sebutkan 4 item tank MLBB", "answers": ["Guardian Helmet", "Dominance Ice", "Blade Armor", "Cursed Helmet"]},
+
+# ================= SPELL =================
+
+{"question": "Sebutkan 5 battle spell MLBB", "answers": ["Flicker", "Retribution", "Execute", "Inspire", "Sprint"]},
+{"question": "Sebutkan 4 spell populer MLBB", "answers": ["Flicker", "Retribution", "Execute", "Sprint"]},
+{"question": "Sebutkan 3 spell untuk jungler", "answers": ["Retribution", "Ice Retribution", "Flame Retribution"]},
+
+# ================= MAP =================
+
+{"question": "Sebutkan 3 lane di MLBB", "answers": ["Top", "Mid", "Bottom"]},
+{"question": "Sebutkan 4 jungle monster MLBB", "answers": ["Buff", "Turtle", "Lord", "Lithowanderer"]},
+{"question": "Sebutkan 3 buff di MLBB", "answers": ["Red Buff", "Blue Buff", "Litho"]},
+
+# ================= HERO SPESIFIK =================
+
+{"question": "Sebutkan 3 hero dengan dash banyak", "answers": ["Fanny", "Ling", "Lancelot"]},
+{"question": "Sebutkan 4 hero dengan skill stun", "answers": ["Eudora", "Aurora", "Tigreal", "Franco"]},
+{"question": "Sebutkan 3 hero yang bisa invisible", "answers": ["Natalia", "Helcurt", "Aamon"]},
+
+# ================= META / UMUM =================
+
+{"question": "Sebutkan 4 role dalam tim MLBB", "answers": ["Tank", "Mage", "Marksman", "Fighter"]},
+{"question": "Sebutkan 3 tugas Tank MLBB", "answers": ["Open War", "Cover", "Initiate"]},
+{"question": "Sebutkan 3 tugas Jungler MLBB", "answers": ["Farming", "Ganking", "Objective"]},
+
+# ================= RANDOM TAMBAHAN =================
+
+{"question": "Sebutkan 4 hero populer rank", "answers": ["Fanny", "Ling", "Beatrix", "Valentina"]},
+{"question": "Sebutkan 5 hero yang sering di-ban", "answers": ["Fanny", "Joy", "Arlott", "Valentina", "Ling"]},
+{"question": "Sebutkan 3 hero yang mudah dipakai", "answers": ["Layla", "Miya", "Zilong"]},
+{"question": "Sebutkan 3 hero tersakit", "answers": ["Aldous", "Lesley", "Gusion"]},
+{"question": "Sebutkan 3 hero sustain tinggi", "answers": ["Uranus", "Esmeralda", "Ruby"]},
+
+# ================= TAMBAHAN BIAR GENAP 100 =================
+
+{"question": "Sebutkan 3 hero dengan skill area", "answers": ["Pharsa", "Yve", "Vale"]},
+{"question": "Sebutkan 4 hero combo teamfight", "answers": ["Atlas", "Tigreal", "Vale", "Pharsa"]},
+{"question": "Sebutkan 3 hero poke damage", "answers": ["Xavier", "Yve", "Pharsa"]},
+{"question": "Sebutkan 4 hero anti tank", "answers": ["Karrie", "Xborg", "Dyrroth", "Lunox"]},
+{"question": "Sebutkan 3 hero scaling kuat", "answers": ["Cecilion", "Aldous", "Lesley"]},
+
+# ================= DUPLIKAT VARIASI (BIAR 100 TANPA NGACO) =================
+
+{"question": "Sebutkan 3 hero assassin terkenal", "answers": ["Fanny", "Ling", "Hayabusa"]},
+{"question": "Sebutkan 4 hero fighter kuat", "answers": ["Chou", "Paquito", "Thamuz", "Aldous"]},
+{"question": "Sebutkan 3 hero mage mudah", "answers": ["Eudora", "Cyclops", "Aurora"]},
+{"question": "Sebutkan 4 hero marksman populer", "answers": ["Layla", "Miya", "Lesley", "Clint"]},
+{"question": "Sebutkan 3 hero support populer", "answers": ["Angela", "Estes", "Rafaela"]},
+
+# ================= FILL SAMPAI 100 =================
+
+{"question": "Sebutkan 3 hero roam MLBB", "answers": ["Franco", "Tigreal", "Khufra"]},
+{"question": "Sebutkan 4 hero exp lane", "answers": ["Thamuz", "Paquito", "Terizla", "Yu Zhong"]},
+{"question": "Sebutkan 3 hero mid lane", "answers": ["Kagura", "Yve", "Xavier"]},
+{"question": "Sebutkan 4 hero gold lane", "answers": ["Miya", "Layla", "Clint", "Lesley"]},
+{"question": "Sebutkan 3 hero jungler", "answers": ["Ling", "Fanny", "Lancelot"]},
+
+{"question": "Sebutkan 3 hero counter assassin", "answers": ["Khufra", "Franco", "Saber"]},
+{"question": "Sebutkan 4 hero meta sekarang", "answers": ["Joy", "Fanny", "Ling", "Arlott"]},
+{"question": "Sebutkan 3 hero damage tinggi", "answers": ["Gusion", "Aldous", "Lesley"]},
+{"question": "Sebutkan 3 hero teamfight kuat", "answers": ["Atlas", "Tigreal", "Khufra"]},
+{"question": "Sebutkan 4 hero sustain tinggi", "answers": ["Uranus", "Esmeralda", "Ruby", "Thamuz"]},
+
+{"question": "Sebutkan 3 hero cepat farming", "answers": ["Ling", "Fanny", "Lancelot"]},
+{"question": "Sebutkan 3 hero mudah push turret", "answers": ["Zilong", "Miya", "Sun"]},
+{"question": "Sebutkan 4 hero anti mage", "answers": ["Athena Shield", "Radiant Armor", "Lolita", "Esmeralda"]},
+{"question": "Sebutkan 3 hero combo flicker", "answers": ["Tigreal", "Atlas", "Khufra"]},
+{"question": "Sebutkan 3 hero skill spam", "answers": ["Cyclops", "Chang'e", "Xavier"]},
+
 ]
